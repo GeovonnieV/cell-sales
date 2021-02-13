89d874b08e91315513baa299a18e118d41635710
geovonnie vasquez <geovonnievasquez@gmail.com>
2021-02-13 10:06:16 -0600

    server packages installed and server is runing on port 3000

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+// connect DB
+
+// import routes
+
+app.listen(port, () => {
+    console.log(`Cell Sells is running on port ${port}`)
+})
