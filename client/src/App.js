dd390017736a1d3753e9c522535a3350e222b93a
geovonnie vasquez <geovonnievasquez@gmail.com>
2021-02-14 15:41:40 -0600

    sass styling homepage begin

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import "./styles/app.scss"
 
 function App() {
   return (
-    <div className="App">
-        <h1>Hello</h1>
+    <div>
+      <div className="home">
+        <div className="home-top-text">
+          <h1>Cellular Sells</h1>
+          <h3>Happy to see you</h3>
+        </div>
+      </div>
+      <div className="homepage-bottom">
+        <div className="cards-container">
+            <div className="card">
+                <figure className="front">
+                  <h1>pro tier</h1>
+                </figure>
+                <figure className="back">
+                  <h1>price</h1>
+                  <button>click me</button>
+                </figure>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
