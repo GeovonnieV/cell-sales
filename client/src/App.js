42c807763773113908e8344ca0b47bc893e4f9d0
unknown <geovonnnievasquez@gmail.com>
2021-02-16 01:16:16 -0600

    card photos added

--- client/src/App.js
+++ client/src/App.js
@@ -14,33 +14,26 @@ function App() {
       {/* bottom */}
       <h2 className="bottom-head">Shop</h2>
       <div className="card-container">
-        <div className="card">
+      {/* card 1 apple*/}
+      <div className="card-apple">
           <div className="card-text">
-            <h2 className="card-title">Apple</h2>
-            <button>Shop</button>  
+            <h2>Apple</h2>
+            <button className="bottom-button" >Shop Apple</button>  
           </div>
-          <div className="card-stats"> </div>
-          {/* end card */}
         </div>
-        {/* card 2 */}
+        {/* card 2 samsung */}
         <div className="card-samsung">
           <div className="card-text">
             <h2>Samsung</h2>
-            <button>click</button>  
+            <button className="bottom-button">Shop Samsung</button>  
           </div>
-          <div className="card-stats"> </div>
-          {/* end card */}
         </div>
-        {/* card 3 */}
-        <div className="card">
-          <div className="card-img"> </div>
+         {/* card 1 apple*/}
+      <div className="card-other">
           <div className="card-text">
-            <span className="date">4 days ago</span>
-            <h2>Post One</h2>
-            <p>lsdjfkls sdjfklj asjkfljkl jkasl fksldjfk  kljskd fj lkjsafl  jklsadfk lkjsadklfjkl klajs f kjlasdfjlk jsdfjoij asdfjkl</p>  
+            <h2>Other</h2>
+            <button className="bottom-button" >Shop Other</button>  
           </div>
-          <div className="card-stats"> </div>
-          {/* end card */}
         </div>
       </div>
     </div>
