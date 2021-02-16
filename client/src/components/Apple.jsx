664261680a298c528f33d6efee59c3fe08386a9b
unknown <geovonnnievasquez@gmail.com>
2021-02-16 16:23:47 -0600

    apple head image

--- client/src/components/Apple.jsx
+++ client/src/components/Apple.jsx
@@ -4,7 +4,15 @@ const Apple = () => {
 
     return(
         <div>
-            <h1 className="apple-heading">Apple</h1>
+            <div className="apple-top">
+                <h1 className="apple-heading">Apple</h1>
+            </div>
+            <div className="apple-row">
+                <h2 className="apple-row-name">Iphones</h2>
+                <div className="apple-card">
+                    
+                </div>
+            </div>
         </div>
     )
 
