a8f868fb2153f48f653c85bc7ecc1b301146f89d
unknown <geovonnnievasquez@gmail.com>
2021-02-24 02:43:19 -0600

    apple component add to cart

--- client/src/components/Apple.jsx
+++ client/src/components/Apple.jsx
@@ -1,62 +1,181 @@
 import React, { useState } from "react";
 import CellNavbar from "./CellNavbar";
 import { Button, Badges, Icons, Dropdown } from "materialize-css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAppleAlt } from '@fortawesome/free-solid-svg-icons'
+import axios from "axios";
 
 const Apple = (props) => {
+  const { iphones, ipads, appleWatches } = props;
 
-    const { iphones, ipads, appleWatches } = props;
+  // state
+  const [iphoneColor, setIphoneColor] = useState("");
+  const [ipadColor, setIpadColor] = useState("");
+  const [watchColor, setWatchColor] = useState("");
 
-    return (
-        <div>
-            <CellNavbar />
-            <h1 className="apple-heading">Apple</h1>
-            <div className="container">
-                <h2 className="apple-row-name">Iphones</h2>
-                <div className="apple-row">
-                    {
-                        iphones.map((iphone) => (
-                            <div className="apple-card">
-                                <img className="card-images" src={iphone.image} alt="" />
-                                <p className="card-item-name">{iphone.name}</p>
-                                <p>${iphone.price.toFixed(2)}</p>
-                                <button className="waves-effect btn-small">Add</button>
-                                {/*  */}
-                            </div>
-                        ))
-                    }
+  // add to cart functions
+
+    // iphone add to cart
+  const addIphoneToCart = (iphone) => {
+    if (iphoneColor === "") {
+      alert("pick a color");
+    } else {
+      axios.post("http://localhost:3000/api/phones", {
+        title: iphone.name,
+        price: iphone.price,
+        color: iphoneColor,
+      });
+
+      alert(`${iphoneColor} ${iphone.name} added to cart`);
+    }
+  };
+
+    // ipad add to cart
+  const addIpadToCart = (ipad) => {
+    if (ipadColor === "") {
+      alert("pick a color");
+    } else {
+      axios.post("http://localhost:3000/api/phones", {
+        title: ipad.name,
+        price: ipad.price,
+        color: ipadColor,
+      });
+
+      alert(`${ipadColor} ${ipad.name} added to cart`);
+    }
+  };
+
+    // watch add to cart
+  const watchAddToCart = (watch) => {
+    if (watchColor === "") {
+      alert("pick a color");
+    } else {
+      axios.post("http://localhost:3000/api/phones", {
+        title: watch.name,
+        price: watch.price,
+        color: watchColor,
+      });
+
+      alert(`${watchColor} ${watch.name} added to cart`);
+    }
+  };
+
+
+
+  return (
+    <div>
+      <CellNavbar />
+      <div className="page-header">
+      <h1 className="apple-heading">Apple</h1>
+      <FontAwesomeIcon icon={faAppleAlt} size="3x" className="apple-icon-top" />
+
+      </div>
+      <div className="container">
+        <h2 className="apple-row-name">Iphones</h2>
+        <div className="apple-row">
+          {iphones.map((iphone) => (
+            <div className="apple-card">
+              <img className="card-images" src={iphone.image} alt="" />
+              <p className="card-item-name">{iphone.name}</p>
+              <p>${iphone.price.toFixed(2)}</p>
+              {/* card btns */}
+              <div className="btns-container">
+                <button
+                  className="waves-effect btn-small"
+                  onClick={() => addIphoneToCart(iphone)}
+                >
+                  Add
+                </button>
+                {/* dropDown */}
+                <div class="dropdown">
+                  <button>Color</button>
+                  <div>
+                    <option onClick={(e) => setIphoneColor(e.target.value)}>
+                      Black
+                    </option>
+                    <option onClick={(e) => setIphoneColor(e.target.value)}>
+                      White
+                    </option>
+                    <option onClick={(e) => setIphoneColor(e.target.value)}>
+                      Gold
+                    </option>
+                  </div>
                 </div>
-                {/* begin ipad row */}
-                <h2 className="apple-row-name">Tablets</h2>
-                <div className="apple-row">
-                    {
-                        ipads.map((ipad) => (
-                            <div className="apple-card">
-                                <img className="card-images" src={ipad.image} alt="" />
-                                <p className="card-item-name">{ipad.name}</p>
-                                <p>${ipad.price.toFixed(2)}</p>
-                                <button className="waves-effect btn-small">Add</button>
-                            </div>
-                        ))
-                    }
-                </div>
-                {/* begin watches row */}
-                <h2 className="apple-row-name">Watches</h2>
-                <div className="apple-row">
-                    {
-                        appleWatches.map((watch) => (
-                            <div className="apple-card">
-                                <img className="watch-images" src={watch.image} alt="" />
-                                <p className="card-item-name">{watch.name}</p>
-                                <p>${watch.price.toFixed(2)}</p>
-                                <button className="waves-effect btn-small">Add</button>
-                            </div>
-                        ))
-                    }
-                </div>
+              </div>
+              {/* end card */}
             </div>
+          ))}
         </div>
-    )
-
+        {/* begin ipad row */}
+        <h2 className="apple-row-name">Ipads</h2>
+        <div className="apple-row">
+          {ipads.map((ipad) => (
+            <div className="apple-card">
+              <img className="card-images" src={ipad.image} alt="" />
+              <p className="card-item-name">{ipad.name}</p>
+              <p>${ipad.price.toFixed(2)}</p>
+                     {/* card btns */}
+                     <div className="btns-container">
+                <button
+                  className="waves-effect btn-small"
+                  onClick={() => addIpadToCart(ipad)}
+                >
+                  Add
+                </button>
+                {/* dropDown */}
+                <div class="dropdown">
+                  <button>Color</button>
+                  <div>
+                    <option onClick={(e) => setIpadColor(e.target.value)}>
+                      Black
+                    </option>
+                    <option onClick={(e) => setIpadColor(e.target.value)}>
+                      White
+                    </option>
+                    <option onClick={(e) => setIpadColor(e.target.value)}>
+                      Gold
+                    </option>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        {/* begin watches row */}
+        <h2 className="apple-row-name">Watches</h2>
+        <div className="apple-row">
+          {appleWatches.map((watch) => (
+            <div className="apple-card">
+              <img className="watch-images" src={watch.image} alt="" />
+              <p className="card-item-name">{watch.name}</p>
+              <p>${watch.price.toFixed(2)}</p>
+                        {/* card btns */}
+                        <div className="btns-container">
+                <button
+                  className="waves-effect btn-small"
+                  onClick={() => watchAddToCart(watch)}
+                >
+                  Add
+                </button>
+                {/* dropDown */}
+                <div class="dropdown">
+                  <button>Color</button>
+                  <div>
+                    <option onClick={(e) => setWatchColor(e.target.value)}>
+                      Black
+                    </option>
+                    <option onClick={(e) => setWatchColor(e.target.value)}>
+                      Silver
+                    </option>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Apple;
